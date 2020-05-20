c7909693656912f6219e6eb20125f64bf85ae61d
Thomas Davis <ThomasDavisA@gmail.com>
2020-05-20 12:20:58 -0700

    added filecontextprovider

--- src/FileContext.js
+++ src/FileContext.js
@@ -1,5 +1,71 @@
 import React from 'react';
 
-const FileContext = React.createContext({});
-
+const FileContext = React.createContext();
 export default FileContext;
+
+export class FileContextProvider extends React.Component {
+    state = {
+        folders: [],
+        notes: []
+    }
+
+    componentDidMount = () => {
+        //add promise all?
+        console.log('didmount fired');
+
+        fetch('http://localhost:9090/folders')
+            .then(res => {return res.json()})
+            .then(folders => {
+                this.setState({
+                    folders: folders
+                })
+            })
+
+        fetch('http://localhost:9090/notes')
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error("bad stuff yo");
+                } return res.json()
+            })
+            .then(notes => {
+                this.setState({
+                    notes: notes
+                })
+            })
+            .catch(error => { return error.message });
+    }
+
+    deleteNote = (noteID) => {
+        fetch(`http://localhost:9090/notes/${noteID}`, {
+            method: 'DELETE',
+            headers: {
+                'content-type': 'application/json'
+            },
+        })
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error("bad stuff yo");
+                } return res.json()
+            })
+            .then(data => {
+                let newNotes = this.state.notes.filter(note =>
+                    note.id !== noteID)
+                this.setState({
+                    notes: newNotes
+                })
+            })
+            .catch(error => { return error.message })
+    }
+
+    render() {
+        return (
+            <FileContext.Provider value={{
+                folders: this.state.folders,
+                notes: this.state.notes,
+                deleteNote: this.deleteNote,
+            }}>
+                {this.props.children}
+            </FileContext.Provider>
+        )
+    }
+};
---
+++ src/api.js
@@ -0,0 +1,29 @@
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/Magdalena';
+
+const getItems = function() {
+ return fetch(`${BASE_URL}/items`);
+};
+
+const createItem = function(name) {
+  let newItem = JSON.stringify({name});
+  return fetch(`${BASE_URL}/items`,
+  {method: 'POST',
+   headers: {'Content-Type': 'application/json'},
+   body: newItem  });
+};
+
+const updateItem = function(id, updateData) {
+    let newData = JSON.stringify(updateData);
+    return fetch(`${BASE_URL}/items/${id}`,
+    {method: 'PATCH',
+     headers: {'Content-Type': 'application/json'},
+     body: newData
+    });
+};
+
+export default {
+    getItems,
+    createItem,
+    updateItem
+};
+
